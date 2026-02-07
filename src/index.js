1f540fb0d43c372a360200d8fdf6280529294823
3-PC <143272974+3-PC@users.noreply.github.com>
2026-02-08 00:06:04 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import dotenv from 'dotenv'
-import express from "express";
 import connectDB from "./db/index.js"; 
 
-const app = express();
 
 dotenv.config()
 
 connectDB()
+.then(() => {
+    app.on("error", () => {
+        throw error; 
+    })
+    app.listen(process.env.PORT || 9000, () => {
+        console.log(`Server connected to mongo and running at port ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.error(err)
+})
 
 /*
 (async () => {
@@ -17,7 +26,7 @@ connectDB()
             throw(error)
         })
 
-        app.listen(process.env.PORT , (req,res) => {
+        app.listen(process.env.PORT , () => {
             console.log(`App listening on port ${process.env.PORT}`)
         })
     }
